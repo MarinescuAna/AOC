96d4e502a3e3c3451d2401312c4a127f2fdef0f2
NRO\a.marinescu <marinescuana28@gmail.com>
2021-12-06 08:56:20 +0200

    Day 6 inspiered by reddit[my solution takes to long]

--- Problems/Day6.cs
+++ Problems/Day6.cs
@@ -6,23 +6,42 @@ namespace AdventOfCode2021.Problems
 {
     public class Day6 : BaseDay
     {
-        public Day6() : base(6, "")
+        public Day6() : base(6, "Lanternfish")
         {
+            ReadInput();
         }
-
-        public override void Part1()
+        private long ReproduceLanternfishs(int DaysPassing)
         {
-            throw new NotImplementedException();
+            long[] LanternfishsTime = new long[9];
+            foreach (var ch in line.Split(','))
+            {
+                LanternfishsTime[int.Parse(ch)]++;
+            }
+
+            for (var t = 0; t < DaysPassing; t++)
+            {
+                LanternfishsTime[(t + 7) % 9] += LanternfishsTime[t % 9];
+            }
+            var sum = (long)0;
+            for (var n = 0; n < LanternfishsTime.Length; n++)
+            {
+                sum += LanternfishsTime[n];
+            }
+            return sum;
+        }
+        public override void Part1()
+        {     
+            Display(ReproduceLanternfishs(80).ToString(), true);
         }
 
         public override void Part2()
         {
-            throw new NotImplementedException();
+            Display(ReproduceLanternfishs(256).ToString(), false);
         }
 
         public override void ReadInput()
         {
-            throw new NotImplementedException();
+            line = StreamReader.ReadLine();
         }
     }
 }
