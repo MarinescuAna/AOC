55040673084e3495cbc98639c601cd9af047c73d
NRO\a.marinescu <marinescuana28@gmail.com>
2021-12-21 16:37:12 +0200

    day 17 part1 done

--- Problems/Day17.cs
+++ Problems/Day17.cs
@@ -6,23 +6,41 @@ namespace AdventOfCode2021.Problems
 {
     public class Day17 : BaseDay
     {
-        public Day17() : base(17, "")
+        private (int, int) x = (0, 0);
+        private (int, int) y = (0, 0);
+
+        public Day17() : base(17, "Trick Shot")
         {
+            ReadInput();
         }
 
+
+        private int ComputeHighestPoz()
+        {
+            var currentPoz = 0;
+
+            for(var speed = (y.Item1 * -1) - 1; speed > 0; speed--)
+            {
+                currentPoz += speed;
+            }
+            return currentPoz;
+        }
         public override void Part1()
         {
-            throw new NotImplementedException();
+            Display(ComputeHighestPoz().ToString(), true);
         }
 
         public override void Part2()
         {
-            throw new NotImplementedException();
+            
         }
 
         public override void ReadInput()
         {
-            throw new NotImplementedException();
+            x = (20, 30); 
+            y = (-10, -5);
+            x = (175, 227);
+            y = (-134, -79);
         }
     }
 }
