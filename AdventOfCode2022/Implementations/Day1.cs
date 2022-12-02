566bd5d6c71195ec29d10c7679a7833dbacfd806
marinescuana11 <marinescuana28@gmail.com>
2022-12-02 08:04:50 +0200

    Refactor day1, Day2 implementation and add template for day3

--- AdventOfCode2022/Implementations/Day1.cs
+++ AdventOfCode2022/Implementations/Day1.cs
@@ -3,7 +3,7 @@ namespace AdventOfCode2022.Implementations
 {
     internal class Day1 :Base, IBase
     {
-        private List<int> reindeerCalories= new List<int>();
+        private List<int> _reindeerCalories= new List<int>();
 
         public void Run()
         {
@@ -13,18 +13,18 @@ namespace AdventOfCode2022.Implementations
         }
         public void Part1()
         {
-            Display(resultPart1: reindeerCalories.Last().ToString());
+            Display(resultPart1: _reindeerCalories.Last().ToString());
         }
 
         public void Part2()
         {
-            Display(resultPart2: reindeerCalories.TakeLast(3).Sum().ToString());
+            Display(resultPart2: _reindeerCalories.TakeLast(3).Sum().ToString());
         }
 
         public void ReadData()
         {
             var iterator = 0;
-            reindeerCalories = streamReader.ReadToEnd().Split(Environment.NewLine).Select(u => new
+            _reindeerCalories = streamReader.ReadToEnd().Split(Environment.NewLine).Select(u => new
             {
                 index = string.IsNullOrEmpty(u) ? ++iterator : iterator,
                 value = string.IsNullOrEmpty(u) ? 0: int.Parse(u)
