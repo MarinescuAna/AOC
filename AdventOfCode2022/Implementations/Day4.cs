3c4569b8b92a7522be1016f3c1c883167dd83837
marinescuana11 <marinescuana28@gmail.com>
2022-12-04 08:33:18 +0200

    Day4 implementation  and template for day5

--- AdventOfCode2022/Implementations/Day4.cs
+++ AdventOfCode2022/Implementations/Day4.cs
@@ -2,24 +2,42 @@
 {
     internal class Day4 : Base, IBase
     {
+        private List<string> _sectionAssignments = new List<string>();
         public void Part1()
         {
-            throw new NotImplementedException();
+            Display(resultPart1: _sectionAssignments.Sum(line => InRange(line.Split(",")[0], line.Split(",")[1],1)).ToString());
         }
 
+        private int InRange(string pair1, string pair2, int part) => part switch
+        {
+            1 => NumberIsInPair(int.Parse(pair1.Split("-")[0]), int.Parse(pair1.Split("-")[1]), int.Parse(pair2.Split("-")[0]), int.Parse(pair2.Split("-")[1])) ||
+                PairIsInPair(int.Parse(pair1.Split("-")[0]), int.Parse(pair1.Split("-")[1]), int.Parse(pair2.Split("-")[0]), int.Parse(pair2.Split("-")[1]))
+                ? 1 : 0,
+            2 => NoOverlap(int.Parse(pair1.Split("-")[0]), int.Parse(pair1.Split("-")[1]), int.Parse(pair2.Split("-")[0]), int.Parse(pair2.Split("-")[1]))? 0:1,
+            _ => 0
+        };
+        private bool PairIsInPair(int pair1X, int pair1Y, int pair2X, int pair2Y) =>
+            (pair1X >= pair2X && pair1Y <= pair2Y) || (pair1X <= pair2X && pair1Y >= pair2Y);
+        private bool NumberIsInPair(int pair1X, int pair1Y, int pair2X, int pair2Y) =>
+            (pair1X == pair1Y && (pair1X - pair2Y) * (pair1X - pair2X) <= 0) || (pair2X == pair2Y && (pair2X - pair1Y) * (pair2X - pair1X) <= 0) ;
         public void Part2()
         {
-            throw new NotImplementedException();
+            Display(resultPart2: _sectionAssignments.Sum(line => InRange(line.Split(",")[0], line.Split(",")[1],2)).ToString());
         }
-
+        private bool NoOverlap(int pair1X, int pair1Y, int pair2X, int pair2Y) =>
+            pair1Y < pair2X || //pair1 is in the pair2 left
+            pair1X > pair2Y || //pair1 is in the pair2 right
+            pair2Y < pair1X || //pair2 is in the pair1 left
+            pair2X > pair1Y; //pair2 is in the pair1 right
         public void ReadData()
         {
-            throw new NotImplementedException();
+            _sectionAssignments = streamReader.ReadToEnd().Split(Environment.NewLine).ToList();
         }
-
         public void Run()
         {
-            throw new NotImplementedException();
+            ReadData();
+            Part1();
+            Part2();
         }
     }
 }
---
+++ AdventOfCode2022/Implementations/Day5.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode2022.Implementations
+{
+    internal class Day5 : Base, IBase
+    {
+        public void Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Part2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
