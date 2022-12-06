8b19f8405772c8d9e2cf5cb34e952486dcac39bb
netrom5 <netrom5@infoland.nl>
2022-12-06 13:56:43 +0200

    Implement day 6 and add template for day 7

--- AdventOfCode2022/Implementations/Day6.cs
+++ AdventOfCode2022/Implementations/Day6.cs
@@ -2,24 +2,39 @@
 {
     internal class Day6 : Base, IBase
     {
+        private string _datastreamBuffer = "";
         public void Part1()
         {
-            throw new NotImplementedException();
+            Display(resultPart1: DetectMarkers(4).ToString());
         }
-
         public void Part2()
         {
-            throw new NotImplementedException();
+            Display(resultPart1: DetectMarkers(14).ToString());
         }
+        private int DetectMarkers(int length)
+        {
+            var startOfPacketMarker = _datastreamBuffer.Substring(0, length);
+            var i = 1;
+
+            while (!IsUnique(startOfPacketMarker, length))
+            {
+                startOfPacketMarker = _datastreamBuffer.Substring(i, length);
+                i++;
+            }
+            return i + length - 1;
+        }
+        private bool IsUnique(string substring, int length) => substring.Distinct().Count() == length;
 
         public void ReadData()
         {
-            throw new NotImplementedException();
+            _datastreamBuffer = streamReader.ReadToEnd();
         }
 
         public void Run()
         {
-            throw new NotImplementedException();
+            ReadData();
+            Part1();
+            Part2();
         }
     }
 }
